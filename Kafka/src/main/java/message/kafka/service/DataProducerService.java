77efa5b918a2cc820c5b9b64287f2bb00d199bf0
Lucas Dias Jorge <78762601+LucasDiasJorge@users.noreply.github.com>
2024-08-08 13:59:56 -0300

    Adding a callback function

--- Kafka/src/main/java/message/kafka/service/DataProducerService.java
+++ Kafka/src/main/java/message/kafka/service/DataProducerService.java
@@ -1,12 +1,19 @@
 package message.kafka.service;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Service;
+
+import java.util.concurrent.CompletableFuture;
+
 
 @Service
 public class DataProducerService {
 
     private final KafkaTemplate<String, String> kafkaTemplate;
+    private Logger logger = LogManager.getRootLogger();
 
     public DataProducerService(KafkaTemplate<String, String> kafkaTemplate) {
         this.kafkaTemplate = kafkaTemplate;
@@ -14,7 +21,16 @@ public class DataProducerService {
 
     // Implementation of send message to topic from Client
     public void sendMessage(String topic, String message) {
-        kafkaTemplate.send(topic, message);
+
+        // Kafka callback Logging (https://www.baeldung.com/spring-kafka)
+        CompletableFuture<SendResult<String, String>> future = kafkaTemplate.send(topic, message);
+        future.whenComplete((result, ex) -> {
+            if (ex == null) {
+                logger.info("Sent message: {} with offset: {} to topic: {}", message, result.getRecordMetadata().offset(), result.getRecordMetadata().topic());
+            } else {
+                logger.error(ex.getMessage(), ex);
+            }
+        });
     }
 
     // Implementation of send message to topicCreated
