f0aa4d504b5383f988dd8f9de2d6887f8cf0dd8a
Lucas Dias Jorge <78762601+LucasDiasJorge@users.noreply.github.com>
2024-08-08 16:09:47 -0300

    Update DataProducerService.java

--- Producer/src/main/java/message/kafka/service/DataProducerService.java
+++ Producer/src/main/java/message/kafka/service/DataProducerService.java
@@ -1,29 +1,31 @@
 package message.kafka.service;
 
+import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Service;
 
+import javax.swing.text.html.parser.Entity;
+import java.io.Serializable;
 import java.util.concurrent.CompletableFuture;
 
 
 @Service
 public class DataProducerService {
 
-    private final KafkaTemplate<String, String> kafkaTemplate;
+    private final KafkaTemplate<String, Serializable> kafkaTemplate;
     private Logger logger = LogManager.getLogger("DataProducerLogger");
 
-    public DataProducerService(KafkaTemplate<String, String> kafkaTemplate) {
+    public DataProducerService(KafkaTemplate<String, Serializable> kafkaTemplate) {
         this.kafkaTemplate = kafkaTemplate;
     }
 
-    // Implementation of send message to topic from Client
-    public void sendMessage(String topic, String message) {
-
+    // Implementation of send message to topicCreated
+    public void sendMessage(Serializable message) {
         // Kafka callback Logging (https://www.baeldung.com/spring-kafka)
-        CompletableFuture<SendResult<String, String>> future = kafkaTemplate.send(topic, message);
+        CompletableFuture<SendResult<String, Serializable>> future = kafkaTemplate.send("json-topic",message);
         future.whenComplete((result, ex) -> {
             if (ex == null) {
                 logger.info("Sent message: {} with offset: {} to topic: {}", message, result.getRecordMetadata().offset(), result.getRecordMetadata().topic());
@@ -31,10 +33,5 @@ public class DataProducerService {
                 logger.error(ex.getMessage(), ex);
             }
         });
-    }
-
-    // Implementation of send message to topicCreated
-    public void sendMessage(String message) {
-        kafkaTemplate.send("TopicCreated", message);
     }
 }
