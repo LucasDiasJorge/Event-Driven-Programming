b6b50d627984f7fd022a228b631fe5a943620d6b
Lucas Dias Jorge <78762601+LucasDiasJorge@users.noreply.github.com>
2024-08-08 11:11:25 -0300

    Create KafkaAdminConfig.java

---
+++ Kafka/src/main/java/message/kafka/config/KafkaAdminConfig.java
@@ -0,0 +1,38 @@
+package message.kafka.config;
+
+import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.KafkaAdmin;
+
+import java.util.HashMap;
+
+@Configuration
+public class KafkaAdminConfig {
+
+    public KafkaProperties properties;
+
+    // @Autorwired
+    public KafkaAdminConfig(KafkaProperties properties) {
+        this.properties = properties;
+    }
+
+    // Get configs from application.yml
+    @Bean
+    public KafkaAdmin kafkaAdmin(){
+        var configs = new HashMap<String, Object>();
+        configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG,properties.getBootstrapServers());
+        return new KafkaAdmin(configs);
+    }
+
+    // Creating new topics automatically
+    @Bean
+    public KafkaAdmin.NewTopics topics(){
+        return new KafkaAdmin.NewTopics(
+                TopicBuilder.name("TopicCreated").partitions(2).replicas(1).build()
+        );
+    }
+
+}
