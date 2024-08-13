6b3971a562dc2c32a63c710f2f7f7dbcb381ee79
Lucas Dias Jorge <78762601+LucasDiasJorge@users.noreply.github.com>
2024-08-13 18:00:16 -0300

    Update RequestInterceptor.java

--- Producer/src/main/java/message/kafka/interceptor/RequestInterceptor.java
+++ Producer/src/main/java/message/kafka/interceptor/RequestInterceptor.java
@@ -2,7 +2,6 @@ package message.kafka.interceptor;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import message.kafka.util.CachedBodyHttpServletRequest;
 import message.kafka.service.DataProducerService;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -10,7 +9,8 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import java.io.Serializable;
-import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
 
 @Component
 public class RequestInterceptor implements HandlerInterceptor {
@@ -24,19 +24,20 @@ public class RequestInterceptor implements HandlerInterceptor {
     }
 
     @Override
-    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
-            throws Exception {
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) {
 
         HttpServletRequest req = request;
 
-        CachedBodyHttpServletRequest cachedBodyRequest = new CachedBodyHttpServletRequest(req);
+        Map<String, Object> map = new HashMap<>();
 
-        // Reading the request body
-        String body = new String(cachedBodyRequest.getInputStream().readAllBytes(), StandardCharsets.UTF_8);
+        map.put("message","message");
+        map.put("topic","topic");
+        map.put("principal",req.getUserPrincipal().getName());
+        map.put("uri",req.getRequestURI());
 
         logger.info("Sending the message to the DataProducerService");
 
-        dataProducerService.sendMessage((Serializable) body);
+        dataProducerService.sendMessage((Serializable) map);
 
         logger.info("Message sent");
 
