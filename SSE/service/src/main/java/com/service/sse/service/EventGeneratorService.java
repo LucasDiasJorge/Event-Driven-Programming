c864f7547d6e485e00469eda87a47ef0ea64e09e
Lucas Dias Jorge <78762601+LucasDiasJorge@users.noreply.github.com>
2024-11-25 16:16:22 -0300

    Initial commit

---
+++ SSE/service/src/main/java/com/service/sse/service/EventGeneratorService.java
@@ -0,0 +1,29 @@
+package com.service.sse.service;
+
+import com.service.sse.controller.EventController;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+@Service
+public class EventGeneratorService {
+    private final EventController eventController;
+
+    public EventGeneratorService(EventController eventController) {
+        this.eventController = eventController;
+    }
+
+    public void start() {
+        ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
+        executorService.scheduleAtFixedRate(() -> {
+            String event = generateEvent();
+            eventController.sendEvent(event);
+        }, 0, 1, TimeUnit.SECONDS);
+    }
+
+    private String generateEvent() {
+        return "Event data";
+    }
+}
