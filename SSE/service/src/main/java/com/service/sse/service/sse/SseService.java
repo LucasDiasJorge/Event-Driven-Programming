3b7ca722b7a7625d1e7f67d1b639b531f5c32c1c
Lucas Dias Jorge <78762601+LucasDiasJorge@users.noreply.github.com>
2024-11-26 16:54:34 -0300

    Integrando com o Kafka e lendo os ultimos eventos

--- SSE/service/src/main/java/com/service/sse/service/sse/SseService.java
+++ SSE/service/src/main/java/com/service/sse/service/sse/SseService.java
@@ -1,13 +1,19 @@
 package com.service.sse.service.sse;
 
+import com.service.sse.kafka.config.KafkaConsumerConfig;
 import com.service.sse.models.events.HandShakeEvent;
 import com.service.sse.models.events.ReadingEvent;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.Collections;
 import java.util.Map;
 
 @Service
@@ -16,21 +22,43 @@ public class SseService {
     Logger logger = LoggerFactory.getLogger(SseService.class);
 
     public void sendRealTimeReading(SseEmitter emitter, long connectionKeep) {
+
+        LocalDateTime connectionTime = LocalDateTime.now();
+        LocalDateTime disconnectTime = connectionTime.plusSeconds(connectionKeep);
+
         try {
             emitter.send(new HandShakeEvent(Map.of()).delivery());
         } catch (IOException e) {
             logger.error("HandShakeEvent error: {}", e.getMessage());
         }
 
+        KafkaConsumer<String, String> consumer = null;
+
         try {
-            for (int i = 0; i < (connectionKeep / 1000); i++) {
-                emitter.send(new ReadingEvent(Map.of(/*Last Readed Tag*/)).delivery());
+            consumer = new KafkaConsumer<String, String>(new KafkaConsumerConfig().consumerFactory().getConfigurationProperties());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        consumer.subscribe(Collections.singleton("Lucas"));
+
+        try {
+            while (LocalDateTime.now().isBefore(disconnectTime)) {
+                ConsumerRecords<String, String> records = consumer.poll(1000);
+                for (ConsumerRecord<String,String> data: records) {
+                    emitter.send(new ReadingEvent(Map.of("epc",data.value())).delivery());
+                }
                 Thread.sleep(1000);
             }
+            consumer.unsubscribe();
+            consumer.close();
             emitter.complete();
         } catch (IOException | InterruptedException ex) {
-            logger.error("ReadingEvent error: {}", ex.getMessage()); // User probably disconnect
+            logger.error("ReadingEvent error: {}", ex.getMessage()); // Usuário provavelmente desconectou
+            consumer.unsubscribe();
+            consumer.close();
             emitter.completeWithError(ex);
         }
     }
 }
+
